da524bf4760915549dd15863f0d40a924d7ce5d7
amin <kermonshaw@gmail.com>
2026-03-16 22:10:32 +0330

    implemented an inefficinet and inaccurate AutoEncdoer

--- src/training.py
+++ src/training.py
@@ -11,32 +11,42 @@ class Dataset:
     def __len__(self): return len(self.x)
     def __getitem__(self, i): return self.x[i],self.y[i]
 
-def evaluate(model, valid_dl, loss_func):
-    pred_list = []
-    label_list = []
+def evaluate(model, valid_dl, loss_func, ae_loss=False):
+    # pred_list = []
+    # label_list = []
+    total_loss = 0
+    count = 0
     model.eval()
     with torch.no_grad():
         for xb,yb in valid_dl:
             pred = model(xb)
-            pred_list.append(pred)
-            label_list.append(yb)
-        preds = torch.cat(pred_list, dim=0)
-        labels = torch.cat(label_list, dim=0)
-        loss = loss_func(preds, labels)
-        report(loss, preds, labels)
+            # pred_list.append(pred)
+            # if ae_loss: label_list.append(xb)
+            # else: label_list.append(yb)
+            n = len(xb)
+            count += n
+            if ae_loss: loss = loss_func(pred,xb).item()*n
+            else: loss = loss_func(pred,yb).item()*n
+            total_loss += loss
+        # preds = torch.cat(pred_list, dim=0)
+        # labels = torch.cat(label_list, dim=0)
+        # loss = loss_func(preds, labels)
+        print(f'loss: {total_loss/count:.5f}')
+        # report(loss, preds, labels)
 
-def fit(model, train_dl, valid_dl, opt, loss_func, epochs):
+def fit(model, train_dl, valid_dl, opt, loss_func, epochs, ae_loss = False):
     print(f"initial performance ===============")
-    evaluate(model, valid_dl, loss_func)
+    evaluate(model, valid_dl, loss_func, ae_loss)
     for epoch in range(epochs):
         model.train()
         for xb,yb in train_dl:
-            loss = loss_func(model(xb), yb)
+            if ae_loss: loss = loss_func(model(xb), xb)
+            else: loss = loss_func(model(xb), yb)
             loss.backward()
             opt.step()
             opt.zero_grad()
         print(f"end of epoch {epoch} performance ===============")
-        evaluate(model, valid_dl, loss_func)
+        evaluate(model, valid_dl, loss_func, ae_loss)
 
 
 def get_dls(train_ds, valid_ds, bs, collate_fn, min_bs = 8):
