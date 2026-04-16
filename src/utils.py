682ecb013976b52b939e67f8ef863f4562cb41be
amin <kermonshaw@gmail.com>
2026-04-16 12:20:01 +0330

    added some network utils to src

--- src/utils.py
+++ src/utils.py
@@ -78,3 +78,77 @@ def in_jupyter():
 def in_notebook():
     "Check if the code is running in a jupyter notebook"
     return in_colab() or in_jupyter()
+
+
+#|export --> utils.net
+import urllib, json
+from urllib.error import HTTPError
+from urllib.parse import urlencode, urlparse, urlunparse
+from urllib.request import Request
+
+
+url_default_headers = {
+    "Accept":
+    "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Cache-Control": "max-age=0",
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Upgrade-Insecure-Requests": "1",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
+}
+
+class _SafeRedirectHandler(urllib.request.HTTPRedirectHandler):
+    def redirect_request(self, req, fp, code, msg, headers, newurl):
+        if code in (307, 308):
+            new_req = Request(newurl, data=req.data, method=req.get_method())
+            for k,v in req.headers.items(): new_req.add_header(k, v)
+        else: new_req = super().redirect_request(req, fp, code, msg, headers, newurl)
+        if new_req and urlparse(newurl).netloc != urlparse(req.full_url).netloc: new_req.remove_header('Authorization')
+        return new_req
+
+def urlopener():
+    _opener = urllib.request.build_opener(_SafeRedirectHandler)
+    _opener.addheaders = list(url_default_headers.items())
+    return _opener
+
+
+def urlquote(url):
+    "Update url's path with `urllib.parse.quote`"
+    subdelims = "!$&'()*+,;="
+    gendelims = ":?#[]@"
+    safe = subdelims+gendelims+"%/"
+    p = list(urlparse(url))
+    p[2] = urllib.parse.quote(p[2], safe=safe)
+    for i in range(3,6): p[i] = urllib.parse.quote(p[i], safe=safe)
+    return urlunparse(p)
+
+def urlwrap(url, data=None, headers=None):
+    "Wrap `url` in a urllib `Request` with `urlquote`"
+    return url if isinstance(url,Request) else Request(urlquote(url), data=data, headers=headers or {})
+
+def urlopen(url, data=None, headers=None, timeout=None, **kwargs):
+    "Like `urllib.request.urlopen`, but first `urlwrap` the `url`, and encode `data`"
+    if kwargs and not data: data=kwargs
+    if data is not None:
+        if not isinstance(data, (str,bytes)): data = urlencode(data)
+        if not isinstance(data, bytes): data = data.encode('ascii')
+    try: return urlopener().open(urlwrap(url, data=data, headers=headers), timeout=timeout)
+    except HTTPError as e: 
+        e.msg += f"\n====Error Body====\n{e.read().decode(errors='ignore')}"
+        raise
+
+
+def urlread(url, data=None, headers=None, decode=True, return_json=False, return_headers=False, timeout=None, **kwargs):
+    "Retrieve `url`, using `data` dict or `kwargs` to `POST` if present"
+    try:
+        with urlopen(url, data=data, headers=headers, timeout=timeout, **kwargs) as u: res,hdrs = u.read(),u.headers
+    except HTTPError as e:
+        e.msg += f"\n====Error Body====\n{e.read().decode(errors='ignore')}"
+        raise
+
+    if decode: res = res.decode()
+    if return_json: res = json.loads(res)
+    return (res,dict(hdrs)) if return_headers else res
